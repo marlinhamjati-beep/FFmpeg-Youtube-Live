269bd2001ff6c00e5870764bb17feb86f6ec8380
marlinhamjati-beep <marlinhamjati@gmail.com>
2025-12-09 13:06:33 +0700

    Update start-live.bat

--- start-live.bat
+++ start-live.bat
@@ -1,1 +1,17 @@
+@echo off
+setlocal
+
+REM Load config
+for /f "usebackq tokens=1,2 delims=:" %%A in ("config.json") do (
+    set "%%A=%%B"
+)
+
+REM Trim quotes & spaces
+set "STREAM_KEY=%stream_key:~1,-1%"
+set "VIDEO_PATH=%video_path:~1,-1%"
+
+REM Loop live jika video_loop true
+:START
+ffmpeg -re -stream_loop -1 -i "%VIDEO_PATH%" -c:v libx264 -preset veryfast -b:v 2500k -c:a aac -b:a 128k -f flv "rtmp://a.rtmp.youtube.com/live2/%STREAM_KEY%"
+pause
 
